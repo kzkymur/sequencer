4222f2eb9890c054050c9929fd2aeb20565ee491
kzkymur <kamilu213@icloud.com>
2025-03-15 18:02:11 +0900

    add workflows

--- vite.app.config.ts
+++ vite.app.config.ts
@@ -1,9 +1,20 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "./example/",
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'example/index.html'),
+        queue: resolve(__dirname, 'example/queue/index.html'),
+        independent: resolve(__dirname, 'example/independent/index.html'),
+      }
+    },
+    outDir: "example-dist"
+  },
   server: {
     port: 8080
   }
