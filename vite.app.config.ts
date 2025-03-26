715a3889b112ab4f3ea8b45c84e2f64a6f201722
kzkymur <kamilu213@icloud.com>
2025-03-27 00:11:54 +0900

    fix base

--- vite.app.config.ts
+++ vite.app.config.ts
@@ -5,6 +5,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "./example/",
+  base: "/sequencer/",
   build: {
     rollupOptions: {
       input: {
